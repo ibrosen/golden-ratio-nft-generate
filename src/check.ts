cbf67ea0a0ef873eb8b38ff9e09aeffad03755de
Ilan Rosen 🌱 <ilanbrianrosen@gmail.com>
2022-10-28 16:45:58 -0700

    oops

--- src/check.ts
+++ src/check.ts
@@ -5,10 +5,9 @@ import { checkProgress } from './utils/check';
 const main = async () => {
     const startId = +process.argv[process.argv.length - 3];
     const numToGenerate = +process.argv[process.argv.length - 2];
-    const folderBatchSize = +process.argv[process.argv.length - 2];
+    const folderBatchSize = +process.argv[process.argv.length - 1];
     console.log(process.argv)
     checkProgress(startId, numToGenerate, folderBatchSize);
-
 }
 
 main()
