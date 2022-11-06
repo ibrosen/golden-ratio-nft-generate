7bc54779a47cc45caa79cc17492070e0438fe24b
Ilan Rosen 🌱 <ilanbrianrosen@gmail.com>
2022-11-05 17:58:52 -0700

    more fixes

--- src/utils/check.ts
+++ src/utils/check.ts
@@ -3,12 +3,12 @@ import fs from 'fs';
 export const checkProgress = async (startId: number, numToCheck: number, folderBatchSize: number) => {
     const basePath = process.cwd() + '/src/out/images';
 
-    for (let i = Math.floor(startId / folderBatchSize); i < Math.floor(numToCheck / folderBatchSize); i++) {
+    for (let i = Math.floor(startId / folderBatchSize); i < Math.floor((startId + numToCheck) / folderBatchSize); i++) {
 
         const size = fs.readdirSync(`${basePath}/${i * folderBatchSize}`).filter(c => c !== '.DS_Store').length;
 
         if (size !== folderBatchSize) {
-            console.log(`📊 [${i * folderBatchSize + size}/${numToCheck}] generated so far`)
+            console.log(`📊 [${i * folderBatchSize + size - startId}/${numToCheck}] generated so far`)
             return i * folderBatchSize + size - startId;
         }
     }
