aa6d7cbcce3db9e9e1d05b8ad1d3089df85cab2f
Ilan Rosen 🌱 <ilanbrianrosen@gmail.com>
2022-11-08 10:53:53 -0800

    remove + fix

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 16
 nvm use 16
-cd golden-ratio-nft-generate
 npm install -g pm2 
 npm install -g yarn
 yarn set version classic
