cc2601a5c1f932b6d9c923cc32f4bed54ef2be09
Ilan Rosen 🌱 <ilanbrianrosen@gmail.com>
2022-11-12 17:01:03 -0800

    run it back

--- generate.sh
+++ generate.sh
@@ -1,3 +1,4 @@
 !#/bin/bash
 yarn setup clearImages $1 100000 10000
+yarn generate --metadata $1 100000 10000
 pm2 start yarn --no-autorestart --name generate -- generate -- --images $1 100000 10000
